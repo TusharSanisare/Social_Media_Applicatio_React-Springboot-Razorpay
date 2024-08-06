bfd2a66c428f6f55327b94078f8cf5ef01856b47
TusharSanisare <121483380+TusharSanisare@users.noreply.github.com>
2024-08-07 01:10:01 +0530

    Implimented Jwt Security validation

---
+++ backend/src/main/java/backend/backend/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package backend.backend.config;
+
+public class JwtConstant {
+  public static final String SECRET_KEY = "traslouvehitusharshisanitulouvesharrassanisajalouveinhipatanaheosko";
+  public static final String JWT_HEADER = "Authorization";
+}
