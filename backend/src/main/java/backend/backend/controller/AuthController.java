1c00a605853f6b61b6548e6caeb51254a9f4fa83
TusharSanisare <121483380+TusharSanisare@users.noreply.github.com>
2024-08-10 01:12:48 +0530

    i hate version ...

--- backend/src/main/java/backend/backend/controller/AuthController.java
+++ backend/src/main/java/backend/backend/controller/AuthController.java
@@ -3,10 +3,13 @@ package backend.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +35,7 @@ public class AuthController {
   @Autowired
   private CustomUserDetailsServiceImplementation customUserDetails;
 
+  @PostMapping("/signup")
   public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws UserException {
 
     String email = user.getEmail();
@@ -61,5 +65,33 @@ public class AuthController {
     AuthResponse res = new AuthResponse(token, true);
 
     return new ResponseEntity<AuthResponse>(res, HttpStatus.CREATED);
+  }
+
+  public ResponseEntity<AuthResponse> signin(@RequestBody User user) {
+    String username = user.getEmail();
+    String password = user.getPassword();
+
+    Authentication authentication = authentication(username, password);
+
+    String token = jwtProvider.generateToken(authentication);
+
+    AuthResponse res = new AuthResponse(token, true);
+
+    return new ResponseEntity<AuthResponse>(res, HttpStatus.ACCEPTED);
+
+  }
+
+  @PostMapping("/signin")
+  private Authentication authentication(String username, String password) {
+    UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+
+    if (userDetails == null) {
+      throw new BadCredentialsException("Invalide Username...");
+    }
+    if (passwordEncoder.matches(password, userDetails.getPassword())) {
+      throw new BadCredentialsException("Invalide Username Or Password");
+    }
+
+    return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
   }
 }
